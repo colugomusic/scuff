ebcc58cc4f3741a9a3de9ebcf18455c985701bc9
Colugo <colugomusic@gmail.com>
2024-11-26 09:39:26 +0000

    Update jthread.hpp

--- common/include/jthread.hpp
+++ common/include/jthread.hpp
@@ -2,6 +2,7 @@
 
 #if defined(__APPLE__)
 #include "third-party/jthread.hpp"
+namespace std { using jthread = nonstd::jthread; using stop_token = nonstd::stop_token; }
 #else
 #include <thread>
 #endif
