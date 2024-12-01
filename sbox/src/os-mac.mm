8b5ad76e38965926b24328ab3c9ae594bfc7b928
Colugo <colugomusic@gmail.com>
2024-12-01 16:08:21 +0000

    Update os-mac.mm

--- sbox/src/os-mac.mm
+++ sbox/src/os-mac.mm
@@ -1,19 +1,14 @@
 #include "common-os.hpp"
+#include "edwin-ext.hpp"
 #include "os.hpp"
-#include <Cocoa/Cocoa.h>
 
 namespace scuff::sbox::os {
 
-auto get_editor_window_native_handle(const sbox::device& dev) -> void* {
-	const auto nsview = (NSView*)(view_native(dev.ui.view));
-	return nsview.window;
-}
-
-auto make_clap_window_ref(View* view) -> clap_window_t {
-	clap_window_t ref;
-	ref.api   = scuff::os::get_clap_window_api();
-	ref.cocoa = view_native(view);
-	return ref;
+auto make_clap_window_ref(edwin::window* wnd) -> clap_window_t {
+    clap_window_t ref;
+    ref.api   = scuff::os::get_clap_window_api();
+    ref.cocoa = edwin::get_nsview(*wnd);
+    return ref;
 }
 
 } // scuff::sbox::os
