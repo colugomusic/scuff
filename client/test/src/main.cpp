013fb710fb133d601f94e6145f1b5d9aa13cb07a
Colugo <colugomusic@gmail.com>
2024-12-05 14:27:30 +0000

    single-sandbox rack test

--- client/test/src/main.cpp
+++ client/test/src/main.cpp
@@ -163,51 +163,97 @@ TEST_CASE("lifetimes") {
 	scuff::create_device_result device1, device2, device3, device4;
 	scuff::id::group group1;
 	scuff::id::sandbox sbox1, sbox2;
-	group1 = scuff::create_group(nullptr);
-	sbox1  = scuff::create_sandbox(group1, sbox_exe_path_.string());
-	scuff::activate(group1, 44100.0);
+	CHECK_NOTHROW(group1 = scuff::create_group(nullptr));
+	CHECK_NOTHROW(sbox1  = scuff::create_sandbox(group1, sbox_exe_path_.string()));
+	CHECK_NOTHROW(scuff::activate(group1, 44100.0));
 	CHECK_NOTHROW(scuff::erase(group1));
 	CHECK_NOTHROW(sbox2 = scuff::create_sandbox(group1, sbox_exe_path_.string()));
 	CHECK_NOTHROW(device1 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
-	REQUIRE(device1.was_loaded_successfully);
+	REQUIRE      (device1.was_loaded_successfully);
 	CHECK_NOTHROW(scuff::erase(sbox1));
 	CHECK_NOTHROW(device2 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
-	REQUIRE(device2.was_loaded_successfully);
+	REQUIRE      (device2.was_loaded_successfully);
 	CHECK_NOTHROW(scuff::erase(device1.id));
 	CHECK_NOTHROW(scuff::erase(device2.id));
-	CHECK_THROWS(device3 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
+	CHECK_THROWS (device3 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
 	CHECK_NOTHROW(scuff::erase(sbox2));
-	CHECK_THROWS(scuff::erase(device3.id));
-	CHECK_THROWS(device4 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
-	CHECK_THROWS(sbox1 = scuff::create_sandbox(group1, sbox_exe_path_.string()));
+	CHECK_THROWS (scuff::erase(device3.id));
+	CHECK_THROWS (device4 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
+	CHECK_THROWS (sbox1 = scuff::create_sandbox(group1, sbox_exe_path_.string()));
 }
 
 TEST_CASE("single-sandbox rack connections") {
 	scuff::create_device_result device1, device2, device3, device4;
 	scuff::id::group group1;
 	scuff::id::sandbox sbox1;
+	CHECK_NOTHROW(group1 = scuff::create_group(nullptr));
+	CHECK_NOTHROW(sbox1  = scuff::create_sandbox(group1, sbox_exe_path_.string()));
+	CHECK_NOTHROW(scuff::activate(group1, 44100.0));
+	CHECK_NOTHROW(device1 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
+	CHECK_NOTHROW(device2 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
+	CHECK_NOTHROW(device3 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
+	CHECK_NOTHROW(device4 = scuff::create_device(sbox1, scuff::plugin_type::clap, {"studio.kx.distrho.MaGigaverb"}));
+	REQUIRE      (device1.was_loaded_successfully);
+	REQUIRE      (device2.was_loaded_successfully);
+	REQUIRE      (device3.was_loaded_successfully);
+	REQUIRE      (device4.was_loaded_successfully);
+	CHECK_NOTHROW(scuff::connect(device1.id, 0, device2.id, 0));
+	CHECK_NOTHROW(scuff::connect(device2.id, 0, device3.id, 0));
+	CHECK_NOTHROW(scuff::connect(device3.id, 0, device4.id, 0));
+	scuff::group_process gp;
+	scuff::audio_input in;
+	scuff::audio_output out;
+	in.dev_id      = device1.id;
+	in.port_index  = 0;
+	in.write_to    = [](float* floats) { for (int i = 0; i < scuff::VECTOR_SIZE * scuff::CHANNEL_COUNT; i++) { floats[i] = 0.0f; } };
+	out.dev_id     = device4.id;
+	out.port_index = 0;
+	out.read_from  = [](const float* floats) {};
+	gp.group = group1;
+	gp.audio_inputs.push_back(in);
+	gp.audio_outputs.push_back(out);
+	gp.input_events.count = [] { return 0; };
+	gp.input_events.pop   = [](size_t, scuff::input_event*) { return 0; };
+	gp.output_events.push = [](const scuff::output_event&) {};
+	CHECK_NOTHROW(scuff::audio_process(gp));
+	// Move the last device to the front ...
+	CHECK_NOTHROW(scuff::disconnect(device3.id, 0, device4.id, 0));
+	// Keep processing audio while the devices are being rewired ...
+	CHECK_NOTHROW(scuff::audio_process(gp));
+	CHECK_NOTHROW(scuff::connect(device4.id, 0, device1.id, 0));
+	CHECK_NOTHROW(scuff::audio_process(gp));
+	gp.audio_inputs[0].dev_id = device4.id;
+	gp.audio_outputs[0].dev_id = device3.id;
+	CHECK_NOTHROW(scuff::audio_process(gp));
+	CHECK_NOTHROW(scuff::erase(device1.id));
+	CHECK_NOTHROW(scuff::erase(device2.id));
+	CHECK_NOTHROW(scuff::erase(device3.id));
+	CHECK_NOTHROW(scuff::erase(device4.id));
+	CHECK_NOTHROW(scuff::erase(sbox1));
+	CHECK_NOTHROW(scuff::erase(group1));
+
 }
 
-TEST_CASE("stress test") {
-	auto group = scuff::managed_group{scuff::create_group(nullptr)};
-	const auto sbox    = scuff::managed_sandbox{scuff::create_sandbox(group.id(), sbox_exe_path_.string())};
-	scuff::activate(group.id(), 44100.0);
-	SUBCASE ("erase the group") {
-		group = {};
-	}
-	SUBCASE ("deactivate group") {
-		scuff::deactivate(group.id());
-	}
-	auto devices = create_a_bunch_of_devices(sbox.id());
-	SUBCASE ("just erase the group") {
-		group = {};
-	}
-	SUBCASE ("erase devices") {
-		SUBCASE ("deactivate group first") {
-			scuff::deactivate(group.id());
-		}
-		for (const auto dev : devices) {
-			scuff::erase(dev);
-		}
-	}
-}
+//TEST_CASE("stress test") {
+//	auto group = scuff::managed_group{scuff::create_group(nullptr)};
+//	const auto sbox    = scuff::managed_sandbox{scuff::create_sandbox(group.id(), sbox_exe_path_.string())};
+//	scuff::activate(group.id(), 44100.0);
+//	SUBCASE ("erase the group") {
+//		group = {};
+//	}
+//	SUBCASE ("deactivate group") {
+//		scuff::deactivate(group.id());
+//	}
+//	auto devices = create_a_bunch_of_devices(sbox.id());
+//	SUBCASE ("just erase the group") {
+//		group = {};
+//	}
+//	SUBCASE ("erase devices") {
+//		SUBCASE ("deactivate group first") {
+//			scuff::deactivate(group.id());
+//		}
+//		for (const auto dev : devices) {
+//			scuff::erase(dev);
+//		}
+//	}
+//}
