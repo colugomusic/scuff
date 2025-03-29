c6dfc438bdf54b08efe112f37a0a4d28bff37e81
Colugo <colugomusic@gmail.com>
2025-03-29 14:03:55 +0000

    Update device-state.hpp

--- client/include/scuff/device-state.hpp
+++ client/include/scuff/device-state.hpp
@@ -28,7 +28,7 @@ struct device_state {
 		}
 		return body_->bytes;
 	}
-	auto operator<=>(const device_state& rhs) const {
+	constexpr auto operator<=>(const device_state& rhs) const {
 		return body_ <=> rhs.body_;
 	}
 	operator bool() const { return bool(body_); }
